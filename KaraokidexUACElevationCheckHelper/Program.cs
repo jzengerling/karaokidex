d43d62c76f8b5aa5cfc9ded8394adda61079d644
msclark1 <msclark1@gmail.com>
2009-05-11 07:33:54 +0000

    Added UAC Elevation Check helper

---
+++ KaraokidexUACElevationCheckHelper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace KaraokidexUACElevationCheckHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appExe = 
+                Environment.GetFolderPath(Environment.SpecialFolder.Programs) + 
+                @"\www.ne0ge0.com\Karaokidex.appref-ms";
+            Process.Start(appExe);
+        }
+    }
+}
